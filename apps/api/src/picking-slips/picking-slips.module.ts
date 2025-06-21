350ff038164e45d741a7b9d307d92640b15f0ade
Zehrei Verj Morales <moraleszehreiverj8@gmail.com>
2025-06-21 17:03:38 +0800

    feat: add cqrs

--- apps/api/src/picking-slips/picking-slips.module.ts
+++ apps/api/src/picking-slips/picking-slips.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
-    imports: [],
+    imports: [CqrsModule],
 })
 export class PickingSlipsModule {}
