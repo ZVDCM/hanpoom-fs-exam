c1c180fe9478cd7a7a7f2d9f7d486926fc4ce989
Zehrei Verj Morales <moraleszehreiverj8@gmail.com>
2025-06-23 01:02:58 +0800

    feat: add swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,9 +4,18 @@ import { AllExceptionsFilter } from 'src/all-exceptions.filter';
 import { SnakeCaseInterceptor } from 'src/snake-case.interceptor';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('Hanpoom FS Exam')
+        .setDescription('Picking Slips and Authentication')
+        .setVersion('1.0')
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
 
     app.use(helmet());
 
