7756141fb3e895f4b7afe7caf0f5519081f0e4ec
Zehrei Verj Morales <moraleszehreiverj8@gmail.com>
2025-06-23 00:35:23 +0800

    feat: add helmet

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,16 +3,19 @@ import { AppModule } from './app.module';
 import { AllExceptionsFilter } from 'src/all-exceptions.filter';
 import { SnakeCaseInterceptor } from 'src/snake-case.interceptor';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.use(helmet());
+
+    app.use(cookieParser());
 
     const { httpAdapter } = app.get(HttpAdapterHost);
     app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
     app.useGlobalInterceptors(new SnakeCaseInterceptor());
-
-    app.use(cookieParser());
 
     await app.listen(process.env.PORT ?? 3001);
 }
